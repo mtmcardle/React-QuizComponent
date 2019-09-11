31dec20809c68f408ee279755676c897d178e8fd
Mike McArdle <mmcardle44@gmail.com>
2019-09-11 13:28:29 -0400

    2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+//import { prependOnceListener } from 'cluster'
+
+class QuizQuestion extends Component {
+    render(props) {
+        return (
+            <main>
+            <section>
+              <p>{this.props.quiz_question.instruction_text}</p>
+            </section>
+            <section className="buttons">
+              <ul>
+                  <li>{this.props.quiz_question.answer_options[0]}</li>
+              </ul>
+            </section>
+          </main>
+        )
+    }
+}
+
+export default QuizQuestion
