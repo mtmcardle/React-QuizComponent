564379ae714a4cbc36a6a50c3436364f2d994ecb
Mike McArdle <mmcardle44@gmail.com>
2019-09-17 09:39:29 -0400

    7

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,14 +16,21 @@ class Quiz extends Component {
     })
   }
 
+  handleResetclick() {
+    this.setState(this.state.quiz_position = 1)
+  }
+
   render() {
 
     const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length)
 
     return (
       <div>
-        {isQuizEnd ? <QuizEnd />
-         : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetclick.bind(this)}/>
+         : <QuizQuestion 
+              quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+              showNextQuestionHandler={this.showNextQuestion.bind(this)} />
+        }
       </div>
     )
   }
